e306c705a175f503960118d1b7da18f355d80bfa
Jônathas Assunção <jaa020399@gmail.com>
2020-12-04 00:32:34 -0300

    commit unversioned Files

---
+++ 4.Sistema/SelfService/src/main/java/com/ufg/inf/ps/selfservice/SelfServiceApplication.java
@@ -0,0 +1,13 @@
+package com.ufg.inf.ps.selfservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SelfServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SelfServiceApplication.class, args);
+	}
+
+}
